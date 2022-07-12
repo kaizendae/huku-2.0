59dcbfedaccc95b953e6dc89d5c925fab52ea487
Heidiki <abdelattie@gmail.com>
2022-07-12 17:13:21 +0100

    add home redirect

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,10 +1,27 @@
+import router from 'next/router';
 import React from 'react';
 
 type Props = { title: string; Icon: any };
 
+function goHere(title: string) {
+  switch (title) {
+    case 'HOME':
+      router.push(`/`);
+      break;
+    // to add others
+    default:
+      break;
+  }
+}
+
 function HeaderItem({ Icon, title }: Props) {
   return (
-    <div className="group flex w-12 cursor-pointer flex-col items-center hover:text-white hover:opacity-100  sm:w-2">
+    <div
+      onClick={() => {
+        goHere(title);
+      }}
+      className="group flex w-12 cursor-pointer flex-col items-center hover:text-white hover:opacity-100  sm:w-2"
+    >
       <Icon className="mb-1 h-8 group-hover:animate-bounce" />
       <p className="tracking-widest opacity-0 group-hover:opacity-100">
         {title}
