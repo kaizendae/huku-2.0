2a1bbc2e7668baa9e8cf0abe3f46fde78fbeec6d
Heidiki <abdelattie@gmail.com>
2022-07-12 14:35:38 +0100

    url

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,8 +10,8 @@ function Nav() {
   return (
     <nav className="relative">
       <div className="absolute top-0 left-0 h-10 w-1/12 bg-gradient-to-r from-[#06202A]" />
-      <div className="flex space-x-10 overflow-x-scroll whitespace-nowrap px-10 text-2xl scrollbar-hide sm:space-x-20 sm:px-20">
-        {Object.entries(requests).map(([key, { title, url }]) => (
+      <div className="flex space-x-10 overflow-x-scroll whitespace-nowrap px-10 text-2xl sm:space-x-20 sm:px-20">
+        {Object.entries(requests).map(([key, { title }]) => (
           <h2
             key={key}
             onClick={() => router.replace(`/?genre=${key}`)}
